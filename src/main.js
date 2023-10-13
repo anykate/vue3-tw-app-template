58c09498401998facb8d30d7af8e7bce3cedce93
Aniket Aryamane <aryamane.aniket@gmail.com>
2023-10-13 09:55:23 +0530

    Adding vue-toastification pinia remixicon npm packages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import App from '@/App.vue'
-import '@/assets/css/style.css'
-import { createApp } from 'vue'
-import router from './router'
+import router from '@/router'
 
-createApp(App).use(router).mount('#app')
+import '@/assets/css/style.css'
+import 'remixicon/fonts/remixicon.css'
+import 'vue-toastification/dist/index.css'
+
+import { createPinia } from 'pinia'
+import { createApp } from 'vue'
+import Toast, { POSITION } from 'vue-toastification'
+
+createApp(App)
+    .use(router)
+    .use(Toast, {
+        // Setting the global default position
+        position: POSITION.BOTTOM_RIGHT,
+    })
+    .use(createPinia())
+    .mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,22 @@
+<script setup>
+import { useCounterStore } from '@/stores/counter'
+
+const store = useCounterStore()
+</script>
+
 <template>
-    <div>
-        <h1>Home</h1>
+    <h1 class="text-4xl mb-3">Home</h1>
+    <div class="flex items-center text-2xl">
+        <i
+            @click="store.increment"
+            class="ri-add-circle-fill cursor-pointer mr-3"
+        ></i>
+
+        {{ store.count }}
+
+        <i
+            @click="store.decrement"
+            class="ri-indeterminate-circle-fill cursor-pointer ml-3"
+        ></i>
     </div>
 </template>
