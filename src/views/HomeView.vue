2e4f23148770331b5928bf3eb00b0c4991535de2
Aniket Aryamane <aryamane.aniket@gmail.com>
2023-10-17 11:56:52 +0530

    ESlint & Prettier

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,15 +8,15 @@ const store = useCounterStore()
     <h1 class="mb-3 text-4xl">Home</h1>
     <div class="flex items-center text-2xl">
         <i
-            @click="store.increment"
             class="ri-add-circle-fill mr-3 cursor-pointer"
+            @click="store.increment"
         ></i>
 
         {{ store.count }}
 
         <i
-            @click="store.decrement"
             class="ri-indeterminate-circle-fill ml-3 cursor-pointer"
+            @click="store.decrement"
         ></i>
     </div>
 </template>
