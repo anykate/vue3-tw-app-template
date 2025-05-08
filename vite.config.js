1903b319c0caa7d20242ad75ee77033f856c615e
Aniket Aryamane <aryamane.aniket@gmail.com>
2025-05-08 14:39:47 +0530

    Adding plugins to vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,12 @@ import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { defineConfig } from 'vite'
+import eslintPlugin from 'vite-plugin-eslint'
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [vue(), tailwindcss()],
+	plugins: [vue(), tailwindcss(), eslintPlugin(), vueDevTools()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
