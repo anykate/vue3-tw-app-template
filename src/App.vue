d1f4a06ae06d4b86bf2a1efc6072880a0bb31920
Aniket Aryamane <aryamane.aniket@gmail.com>
2025-05-22 12:33:58 +0530

    Adding route key - App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,19 @@
 
 <template>
 	<div>
-		<router-view v-slot="{ Component }">
-			<transition
+		<router-view v-slot="{ Component, route }">
+			<Transition
+				name="custom-classes"
 				enter-active-class="animate__animated animate__fadeIn"
 				leave-active-class="animate__animated animate__fadeOut"
+				:duration="{ enter: 4000, leave: 10 }"
 			>
-				<component :is="Component" />
-			</transition>
+				<div :key="route.name">
+					<component :is="Component" />
+				</div>
+			</Transition>
 		</router-view>
 	</div>
 </template>
 
-<style>
-.animate__animated.animate__fadeIn,
-.animate__animated.animate__fadeOut {
-	--animate-duration: 200ms;
-}
-</style>
+<style></style>
